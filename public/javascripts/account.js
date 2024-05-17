4a8c555a09a9b2ecf6b29fc42b5d159505813770
Muhammed Shafeek <shafiiq688@gmail.com>
2024-05-17 15:08:50 +0530

    Fixed error on account page that valiadation error while entering phone number or email

--- public/javascripts/account.js
+++ public/javascripts/account.js
@@ -1,5 +1,7 @@
+var emailOrPhone;
+
 // email or phone number input setup 
-document.getElementById('emailOrPhone').addEventListener('input', function() {
+document.getElementById('emailOrPhone').addEventListener('input', function () {
 
     var input = this.value.trim();
     var checkIn = document.getElementById('emailOrPhone')
@@ -11,23 +13,28 @@ document.getElementById('emailOrPhone').addEventListener('input', function() {
     } else if (phoneRegex.test(input)) {
         this.removeAttribute('name');
         this.setAttribute('name', 'phoneNumber');
+        emailOrPhone = /^[6789]\d{9}$/;
+        console.log('email or phone', emailOrPhone);
     } else if (emailRegex.test(input)) {
         this.removeAttribute('name');
         this.setAttribute('name', 'email');
+        emailOrPhone = /^([a-zA-Z0-9\.-]+)@(gmail).(com)/;
+        console.log('email or phone', emailOrPhone);
     } else {
         this.removeAttribute('name');
     }
 });
 
 //sign in validation
-document.addEventListener("DOMContentLoaded", function(event) {
-    
-});
-var emailSign = document.getElementById('emailOrPhone');
-var passwordSign = document.getElementById('password');
-var emailFormat = /^([a-zA-Z0-9\.-]+)@(gmail).(com)/;
+document.addEventListener("DOMContentLoaded", function (event) {
 
-emailSign.addEventListener('input', liveValidationSign);
+});
+
+var inputElement = document.getElementById('emailOrPhone');
+var passwordSign = document.getElementById('password');
+
+
+inputElement.addEventListener('input', liveValidationSign);
 passwordSign.addEventListener('input', liveValidationSign);
 
 function liveValidationSign() {
@@ -44,17 +51,29 @@ function validateEmail(value) {
     var requiredError = errorDiv.querySelector('.email-required');
     var formatError = errorDiv.querySelector('.email-format');
     var serverErr = document.getElementById('server-error-message')
-    
+
+    var inputElement = document.getElementById('emailOrPhone');
+    var nameAttribute = inputElement.getAttribute('name');
+    console.log(nameAttribute);
+
     requiredError.style.display = formatError.style.display = serverErr.style.display = 'none';
-    
+
     if (value === "") {
         validEmail = false;
         document.getElementById('emailOrPhone').style.border = "1px solid var(--accent-red)"
         requiredError.style.display = 'flex';
-    } else if (!emailFormat.test(value)) {
+    } else if (!emailOrPhone.test(value)) {
         validEmail = false;
+
         document.getElementById('emailOrPhone').style.border = "1px solid var(--accent-red)"
         formatError.style.display = 'flex';
+
+        // checking inputing is phone number or email 
+        if (nameAttribute === 'phoneNumber') {
+            formatError.innerHTML = 'Enter valid phone Number';
+        } else {
+            formatError.innerHTML = 'Enter valid email address';
+        }
     } else {
         validEmail = true;
         document.getElementById('emailOrPhone').style.border = "1px solid var(--accent-green)"
@@ -68,9 +87,9 @@ function validatePassword(value) {
     var requiredError = errorDiv.querySelector('.password-required');
     var minLengthError = errorDiv.querySelector('.password-min-length');
     var serverErr = document.getElementById('server-error-message');
-    
+
     requiredError.style.display = minLengthError.style.display = serverErr.style.display = 'none';
-    
+
     if (value === "") {
         validPassword = false;
         document.getElementById('password').style.border = "1px solid var(--accent-red)"
